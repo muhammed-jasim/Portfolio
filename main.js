65306908f946c175b038c0f9c5c3242099869a74
muhammed-jasim <muhammedjasim1000@gmail.com>
2024-01-14 12:42:20 +0530

    some content add on portfolio section

--- main.js
+++ main.js
@@ -54,3 +54,20 @@ const  first_open = document.getElementsByClassName('frondend ')
     }
 
 
+    document.getElementById('openModalBtn').addEventListener('click', function() {
+      document.getElementById('modal').style.display = 'block';
+  });
+  
+  document.getElementById('closeModalBtn').addEventListener('click', function() {
+      document.getElementById('modal').style.display = 'none';
+  });
+  
+  // Close the modal if the user clicks outside the modal content
+  window.addEventListener('click', function(event) {
+      var modal = document.getElementById('modal');
+      if (event.target == modal) {
+          modal.style.display = 'none';
+      }
+  });
+  
+    
